de65a3ffa5ddb860c0762c50f6b4165b0513a6cc
alejolo311 <alejolo311@gmail.com>
2020-02-05 12:32:42 +0000

    the files and protos

---
+++ 103-merge_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * merge_sort - The merge sort
+ * Description: merge sort
+ * @array: array to sort
+ * @size: size of the array
+ * Return: void
+ */
+void merge_sort(int *array, size_t size)
+{
+    return 0;
+}
---
+++ 105-radix_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * radix_sort - The radix sort
+ * Description: radix sort
+ * @array: array to sort
+ * @size: size of the array
+ * Return: void
+ */
+void radix_sort(int *array, size_t size)
+{
+    return 0;
+}
