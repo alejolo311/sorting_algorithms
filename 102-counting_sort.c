9db6036106fb8b81ed8909e2b0e83a90799f6ed5
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-02-06 04:15:02 +0000

    Fixing errors

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -14,22 +14,29 @@ void counting_sort(int *array, size_t size)
 	int range = max + 1;
 	int *count = malloc(sizeof(int) * range);
 	int *output = malloc(sizeof(int) * size);
-	int i;
+	int i = 0;
 
 	for (i = 0; i < range; i++)
 		count[i] = 0;
+
 	for (i = 0; i < (int) size; i++)
 		++count[array[i]];
+
 	for (i = 1; i < range; i++)
 		count[i] += count[i - 1];
+
 	print_array(count, range);
-	for (i = 0; array[i]; i++)
+
+	for (i = 0; i < (int) size; i++)
 	{
 		output[count[array[i]] - 1] = array[i];
 		--count[array[i]];
 	}
-	for (i = 0; output[i]; i++)
+
+	for (i = 0; i < (int) size; i++)
 		array[i] = output[i];
+	free(count);
+	free(output);
 }
 
 /**
@@ -44,7 +51,7 @@ int max_num(int *array, size_t size)
 	int max;
 
 	if (size == 0)
-		exit(0);
+		return (0);
 
 	max = array[0];
 	for (i = 0; i < size; i++)
